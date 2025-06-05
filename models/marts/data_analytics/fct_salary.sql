02983b6b630ac4738af9e9681e827773d94a31eb
Boris PAWLIK <156550341+CallMeBoboUI@users.noreply.github.com>
2025-06-05 10:18:14 +0200

    Create fct_salary.sql

---
+++ models/marts/data_analytics/fct_salary.sql
@@ -0,0 +1,13 @@
+select
+    {{ dbt_utils.generate_surrogate_key(['work_year', 'job_title', 'employee_residence', 'company_location']) }} as salary_fact_id,
+    work_year,
+    job_title,
+    employee_residence,
+    company_location,
+    salary,
+    salary_currency,
+    salary_in_usd,
+    remote_ratio,
+    experience_level,
+    employment_type
+from {{ ref('stg_salary_data') }}
